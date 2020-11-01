0ed81a4818b563816be42038f0af360ddef79d50
tiiiiiiido <73116573+tiiiiiiido@users.noreply.github.com>
2020-11-01 09:27:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,95 +1,60 @@
+import datetime
+day = datetime.datetime.now()
+print(day.strftime("%A"))
 from datetime import datetime
 import webbrowser
 import time
-import tkinter as tk
-import threading 
 
-root= tk.Tk()
-
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-canvas1.pack()
-
-def day1 ():  
+if day.strftime("%A") == "Sunday":
     with open('data/days/day1.txt', 'r') as day:
-        global links
-        links = day.readlines()
-        
-        whileloop()
-def day2 ():  
+        print("today is sunday")
+        links = day.readlines()      
+elif day.strftime("%A") == "Monday":
     with open('data/days/day2.txt', 'r') as day:
-        global links
-        links = day.readlines()
-        whileloop()
-def day3 ():  
+        print("today is Monday")            
+        links = day.readlines()      
+elif day.strftime("%A") == "Tuesday":
     with open('data/days/day3.txt', 'r') as day:
-        global links
-        links = day.readlines()
-        whileloop()
-def day4 ():  
+        print("today is Tuesday")            
+        links = day.readlines()  
+elif day.strftime("%A") == "Wednesday":
     with open('data/days/day4.txt', 'r') as day:
-        global links
-        links = day.readlines()
-        whileloop()
-def day5 ():  
+        print("today is Wednesday")            
+        links = day.readlines()     
+elif day.strftime("%A") == "Thursday":
     with open('data/days/day5.txt', 'r') as day:
-        global links
-        links = day.readlines()
-        whileloop()
-def day6 ():  
+        print("today is Thursday")           
+        links = day.readlines()    
+elif day.strftime("%A") == "Friday": 
     with open('data/days/day6.txt', 'r') as day:
-        global links
-        links = day.readlines()
-        whileloop()
-def day7 ():  
+        print("today is Friday") 
+        links = day.readlines()      
+elif day.strftime("%A") == "Saturday":
     with open('data/days/day7.txt', 'r') as day:
-        global links
+        print("today is Saturday")
         links = day.readlines()
-        whileloop()
 
-button1 = tk.Button(text='monday/الأحد',command=day1, bg='black',fg='white')
-canvas1.create_window(150, 60, window=button1)
+while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M") #cals the time
 
-button2 = tk.Button(text='tuesday/الاثنين',command=day2, bg='black',fg='white')
-canvas1.create_window(150, 90, window=button2)
+    if current_time == "08:15":
+        webbrowser.open(links[0])  #link 1
+        time.sleep(120)  
 
-button3 = tk.Button(text='wednesday/الثلاثاء',command=day3, bg='black',fg='white')
-canvas1.create_window(150, 120, window=button3)
+    elif current_time == "09:03":
+        webbrowser.open(links[1])  #link 2
+        time.sleep(120)  
 
-button4 = tk.Button(text='thursday/الاربعاء',command=day4, bg='black',fg='white')
-canvas1.create_window(150, 150, window=button4)
+    elif current_time == "09:53":
+        webbrowser.open(links[2])  #link 3
+        time.sleep(120)  
 
-button5 = tk.Button(text='friday/الخميس',command=day5, bg='black',fg='white')
-canvas1.create_window(150, 180, window=button5)
-
-button6 = tk.Button(text='saturday/الجمعه',command=day6, bg='black',fg='white')
-canvas1.create_window(150, 210, window=button6)
-
-button6 = tk.Button(text='sunday/السبت',command=day7, bg='black',fg='white')
-canvas1.create_window(150, 210, window=button6)
-
-def whileloop():
-    while True:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M")
-
-        if current_time == "08:15":
-            webbrowser.open(links[0])  #link 1
-            time.sleep(120)  
-
-        elif current_time == "09:03":
-            webbrowser.open(links[1])  #link 2
-            time.sleep(120)  
-
-        elif current_time == "09:53":
-            webbrowser.open(links[2])  #link 3
-            time.sleep(120)  
-
-        elif current_time == "11:08":
-            webbrowser.open(links[3])  #link 4
-            time.sleep(120)  
-        
-        elif current_time == "11:58":
-            webbrowser.open(links[4])  #link 5
-            time.sleep(120)  
-        time.sleep(10)
-root.mainloop()
+    elif current_time == "11:08":
+        webbrowser.open(links[3])  #link 4
+        time.sleep(120)  
+    
+    elif current_time == "11:58":
+        webbrowser.open(links[4])  #link 5
+        time.sleep(120)  
+    time.sleep(10)
